2d7055ab5733874acfb28e2e22949b619bf51b3a
Hajesha <hajesha1@gmail.com>
2020-10-14 22:19:12 -0400

    hashtag

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import nltk
+import re
 from nltk.corpus import stopwords
 import pandas as pd
 
@@ -8,4 +9,7 @@ def readfile(filepath):
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     df_pd = readfile("crowdflower-brands-and-product-emotions/data/judge_1377884607_tweet_product_company.csv")
-    print(df_pd.head(5))
+    df_pd = df_pd.dropna(subset=['tweet_text'])
+    df_pd = df_pd.dropna(subset=['is_there_an_emotion_directed_at_a_brand_or_product'])
+    df_pd['hashtag'] = df_pd['tweet_text'].apply(lambda x: re.findall(r"#(\w+)", x))
+    print(df_pd.head(10))
