d6a5f0087775cbb830a7eb77a0d569f87f809f57
Nicholas Dhanraj <nickjob_dhanraj@hotmail.com>
2020-11-13 15:35:06 -0500

    heh
    heh

--- svm.py
+++ svm.py
@@ -7,11 +7,11 @@ from sklearn.model_selection import train_test_split
 from sklearn import svm
 # Import scikit-learn metrics module for accuracy calculation
 from sklearn import metrics
+from sklearn.preprocessing import MinMaxScaler
+
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import MinMaxScaler
-from sklearn.metrics import precision_score
-from sklearn.metrics import recall_score
+
 
 if __name__ == '__main__':
 
@@ -36,11 +36,11 @@ if __name__ == '__main__':
     # print the cancer labels (0:malignant, 1:benign)
     # print(data.target)
     label = data['emotion']
-    features = data[['hashtags', 'text', 'emojis']]
+    features = data[['hashtags', 'text', 'emojis', 'exaggerate_punctuation']]
     features = features.apply(lambda col: LabelEncoder().fit_transform(
         col.astype(str)), axis=0, result_type='expand')
 
-    msk = np.random.rand(len(features)) < 0.75
+    msk = np.random.rand(len(features)) < 0.7
     traindata_x = features[msk]
     traindata_y = label[msk].values.tolist()
     testdata_x = features[~msk]
@@ -50,22 +50,24 @@ if __name__ == '__main__':
     traindata_x = scaling.transform(traindata_x)
     testdata_x = scaling.transform(testdata_x)
 
-    print("hi1")
-
     # Train the model using the training sets
     clf.fit(traindata_x, traindata_y)
-    print("hi2")
 
     # Predict the response for test dataset
     y_pred = clf.predict(testdata_x)
-    print("hi3")
     # Model Accuracy: how often is the classifier correct?
     print("Accuracy:", metrics.accuracy_score(testdata_y, y_pred))
 
     # Model Precision: what percentage of positive tuples are labeled as such?
-    print("Precision Score : ", precision_score(testdata_y, y_pred, y_pred,
-                                                pos_label='positive',
-                                                average='micro'))
-    print("Recall Score : ", recall_score(testdata_y, y_pred, y_pred,
-                                          pos_label='positive',
-                                          average='micro'))
+    print("Precision Score : ", metrics.precision_score(testdata_y, y_pred,
+                                                        pos_label='positive',
+                                                        average='weighted', zero_division=0))
+    print("Recall Score : ", metrics.recall_score(testdata_y, y_pred,
+                                                  pos_label='positive',
+                                                  average='weighted'))
+
+    print(metrics.f1_score(testdata_y, y_pred, average='macro'))
+
+    print(metrics.f1_score(testdata_y, y_pred, average='micro'))
+
+    print(metrics.f1_score(testdata_y, y_pred, average='weighted'))
