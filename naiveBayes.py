0ffa5b6992b452ca2e97b54a5dde261752311cd4
Hajesha <hajesha1@gmail.com>
2020-12-03 01:32:59 -0500

    not really worth commiting but ill put it up there

--- naiveBayes.py
+++ naiveBayes.py
@@ -1,8 +1,13 @@
 import pandas as pd
 import numpy as np
-from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
+from sklearn.naive_bayes import MultinomialNB, GaussianNB, BernoulliNB, ComplementNB, CategoricalNB
 from sklearn.preprocessing import LabelEncoder
 from sklearn import metrics
+
+
+# REFERENCES FOR ME
+# https://stackoverflow.com/questions/3473612/ways-to-improve-the-accuracy-of-a-naive-bayes-classifier
 
 if __name__ == '__main__':
     # read the data
@@ -10,20 +15,19 @@ if __name__ == '__main__':
     # features = data.drop(['original_text','emotion', 'exaggerate_punctuation'], axis=1)
     features = data[['text', 'hashtags', 'emojis',
                      'exaggerate_punctuation', 'pos_tag']]
+    # features = data[['text']]
     features = features.apply(lambda col: LabelEncoder().fit_transform(
         col.astype(str)), axis=0, result_type='expand')
-    # TODO fix this
     label = data['emotion']
-    # label = label.apply(lambda col: LabelEncoder().fit_transform(col.astype(str)), axis=0, result_type='expand')
 
     # split the data into 75% training and 25% testing
-    msk = np.random.rand(len(features)) < 0.75
+    msk = np.random.rand(len(features)) < 0.95
     traindata_x = features[msk]
     traindata_y = label[msk].values.tolist()
     testdata_x = features[~msk]
     testdata_y = label[~msk].values.tolist()
 
-    classifier = MultinomialNB()
+    classifier = BernoulliNB()
 
     # fit the model
     classifier.fit(traindata_x, traindata_y)
@@ -34,3 +38,20 @@ if __name__ == '__main__':
     print("Accuracy:", metrics.accuracy_score(testdata_y, pred))
     print("\nClassification Report:",
           metrics.classification_report(testdata_y, pred, zero_division=0))
+
+
+    clf_4 = AdaBoostClassifier()
+    clf_4.fit(traindata_x, traindata_y)
+    pred2 = clf_4.predict(testdata_x)
+
+    print("Accuracy:", metrics.accuracy_score(testdata_y, pred2))
+    print("\nClassification Report:",
+          metrics.classification_report(testdata_y, pred2, zero_division=0))
+
+    clf_4 = RandomForestClassifier()
+    clf_4.fit(traindata_x, traindata_y)
+    pred2 = clf_4.predict(testdata_x)
+
+    print("Accuracy:", metrics.accuracy_score(testdata_y, pred2))
+    print("\nClassification Report:",
+          metrics.classification_report(testdata_y, pred2, zero_division=0))
---
+++ plotting.py
@@ -0,0 +1,26 @@
+
+import pandas as pd
+from sklearn.utils import resample
+
+def readfile(filepath):
+    return pd.read_csv(filepath, encoding='utf-8', memory_map=True)
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    df_pd = readfile("results.csv").sample(frac=1,random_state=4)
+    df_majority = df_pd[df_pd.emotion == 1]
+    df_minority = df_pd[df_pd.emotion == -1]
+    print(df_pd.emotion.value_counts())
+    # df_minority_upsampled = resample(df_majority,
+    #                                  replace=False,     # sample with replacement
+    #                                  n_samples=7613,    # to match majority class
+    #                                  random_state=123)
+    #
+    # df_upsampled = pd.concat([df_minority_upsampled, df_minority])
+
+    df_majority_sample = df_pd.loc[df_pd['emotion'] == 1].sample(n=10613, random_state=42)
+    df_sampled = pd.concat([df_majority_sample, df_minority])
+    print(df_sampled.emotion.value_counts())
+
+    df_sampled.to_csv('resultsupscaled.csv', encoding='utf-8')
